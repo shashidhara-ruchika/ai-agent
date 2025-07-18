402fde82a7ef71571a22315727e1319df4fb0f13
shashidhara-ruchika <shashidhara.r@northeastern.edu>
2025-07-18 12:43:39 -0400

    add memeory from json file

--- src/llm.ts
+++ src/llm.ts
@@ -1,13 +1,14 @@
 import dotenv from "dotenv";
 import { openai } from './ai'
+import type OpenAI from 'openai'
 
 dotenv.config();
 
-export const runLLM = async ({ userMessage }: { userMessage: string }) => {
+export const runLLM = async ({ messages }: { messages: OpenAI.Chat.Completions.ChatCompletionMessageParam[] }) => {
   const response = await openai.chat.completions.create({
     model: "openai/gpt-4o-mini", 
     temperature: 0.1,
-    messages: [{ role: "user", content: userMessage }],
+    messages,
   });
 
   return response.choices[0].message.content;
